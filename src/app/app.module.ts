cca91c8b3085d7ff5071830e27450b5fc1e7d69a
Laxman Kumar <laxman.apr9@gmail.com>
2018-08-11 11:52:53 +0530

    Added Components and Templates Pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing/app-routing.module';
+import { ComponentsTemplatesModule } from './components-templates/components-templates.module';
 import { AppComponent } from './app.component';
 import { IndexComponent } from './index/index.component';
 
@@ -13,7 +14,8 @@ import { IndexComponent } from './index/index.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgbModule.forRoot()
+    NgbModule.forRoot(),
+    ComponentsTemplatesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
